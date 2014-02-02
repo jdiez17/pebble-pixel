099dbea556f5170a20c4f32342de9dae1fdfc055
jdiez17 <jose.manuel.diez@gmail.com>
2014-02-02 15:09:31 +0000

    Updated code to v2 SDK

--- src/main.c
+++ src/main.c
@@ -1,20 +1,12 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
+#include <time.h>
 
-#define MY_UUID { 0xDE, 0xEF, 0xAD, 0xDE, 0xFE, 0xAC, 0xBE, 0xEF, 0x99, 0xEF, 0xBE, 0xEF, 0xBE, 0xEF, 0xBE, 0xEF }
-PBL_APP_INFO(MY_UUID,
-    "Pixel Watch",
-    "Jdiez",
-    1, 0,
-    DEFAULT_MENU_ICON,
-    APP_INFO_WATCH_FACE);
-    
-Window window;
-BmpContainer background;
+Window* window;
+BitmapLayer* background;
+Layer* w_layer;
 
-BmpContainer numbers[4];
-int states[4] = {-1, -1, -1, -1};
+BitmapLayer* numbers[4];
+GBitmap* loaded_bitmaps[4] = {NULL, NULL, NULL, NULL};
 int resources[10] = {
     RESOURCE_ID_IMAGE_ZERO,
     RESOURCE_ID_IMAGE_ONE, 
@@ -31,75 +23,74 @@ int resources[10] = {
 void unload_number(int row, int pos) {
     int idx = 2 * row + pos;
 
-    if(states[idx] != -1) {
-        layer_remove_from_parent(&numbers[idx].layer.layer);
-        bmp_deinit_container(&numbers[idx]);
-        states[idx] = -1;
+    if(loaded_bitmaps[idx] != NULL)  {
+        gbitmap_destroy(loaded_bitmaps[idx]);
+        layer_remove_from_parent(bitmap_layer_get_layer(numbers[idx]));
+        bitmap_layer_destroy(numbers[idx]);
+
+        loaded_bitmaps[idx] = NULL;
     }
-} 
+}
     
 void load_number(int row, int pos, int resid) {
     int idx = 2 * row + pos;
 
     unload_number(row, pos);
     
-    states[idx] = resid;
-    bmp_init_container(resid, &numbers[idx]);
-    numbers[idx].layer.layer.frame.origin.x = (pos == 0) ? 25 : 77;
-    numbers[idx].layer.layer.frame.origin.y = (row == 0) ? 11 : 89;
-    layer_add_child(&window.layer, &numbers[idx].layer.layer);
+    loaded_bitmaps[idx] = gbitmap_create_with_resource(resources[resid]);
+    GRect frame = (GRect) {
+        .origin = {
+            .x = (pos == 0) ? 25 : 77,
+            .y = (row == 0) ? 11 : 89
+        }, 
+        .size = loaded_bitmaps[idx]->bounds.size
+    };
+    numbers[idx] = bitmap_layer_create(frame);
+    bitmap_layer_set_bitmap(numbers[idx], loaded_bitmaps[idx]);
+    layer_add_child(w_layer, bitmap_layer_get_layer(numbers[idx]));
 }
 
-void show_time(PblTm* t) {
+void show_time(struct tm* t) {
     unsigned short hour = (clock_is_24h_style() ? t->tm_hour : (t->tm_hour > 12 ? t->tm_hour - 12 : t->tm_hour));
     
-    load_number(0, 0, resources[hour / 10]);
-    load_number(0, 1, resources[hour % 10]);
-    load_number(1, 0, resources[t->tm_min / 10]);
-    load_number(1, 1, resources[t->tm_min % 10]);
+    load_number(0, 0, hour / 10);
+    load_number(0, 1, hour % 10);
+    load_number(1, 0, t->tm_min / 10);
+    load_number(1, 1, t->tm_min % 10);
 }
 
-void handle_init(AppContextRef ctx) {
-    (void) ctx;
-    
-    window_init(&window, "Pixel Watch");
-    window_stack_push(&window, true);
-    
-    resource_init_current_app(&PIXELWATCH);
-    
-    bmp_init_container(RESOURCE_ID_IMAGE_BACKGROUND, &background);
-    layer_add_child(&window.layer, &background.layer.layer);
-    
-    PblTm t;
-    get_time(&t);
-    show_time(&t);
+void handle_minute_tick(struct tm* t, TimeUnits delta_t) {
+    show_time(t);
 }
 
-void handle_deinit(AppContextRef ctx) {
-    (void) ctx;
+void handle_init(void) {
+    window = window_create();
+    window_stack_push(window, true);
+    w_layer = window_get_root_layer(window);
+
+    background = bitmap_layer_create(layer_get_frame(w_layer));
+    bitmap_layer_set_bitmap(background, gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND));
+    layer_add_child(w_layer, bitmap_layer_get_layer(background));
+
+    tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+
+    time_t now = time(NULL);
+    struct tm* t = localtime(&now);
+    show_time(t);
+}
+
+void handle_deinit(void) {
+    window_destroy(window);
+    bitmap_layer_destroy(background);
     
     for(int i = 0; i < 4; i++)
         unload_number(i >> 1, i & 1);
     
-    bmp_deinit_container(&background);
+    tick_timer_service_unsubscribe();
 }
 
-void handle_minute_tick(AppContextRef ctx, PebbleTickEvent* t) {
-    (void) ctx;
-    
-    show_time(t->tick_time);
-}
-
-void pbl_main(void *params) {
-    PebbleAppHandlers handlers = {
-        .init_handler = &handle_init,
-        .deinit_handler = &handle_deinit,
-        
-        .tick_info = {
-            .tick_handler = &handle_minute_tick,
-            .tick_units = MINUTE_UNIT
-        }
-    };
-    
-    app_event_loop(params, &handlers);
+int main(void) {
+    handle_init();
+    app_event_loop();
+    handle_deinit();
 }
